411912dfd054ad87088ed9040941e43fb4e9958a
Rosha Vafaee <roshaavafaeii@gmail.com>
2026-07-27 12:13:24 -0700

    Add solution for Valid Parentheses

---
+++ stack/0020-valid-parentheses/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    private final List<Character> leftBrackets = Arrays.asList('(', '[', '<', '{');
+    private final List<Character> rightBrackets = Arrays.asList(')', ']', '>', '}');
+
+    public boolean isValid(String s) {
+           Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (isLeftBracket(ch))
+                stack.push(ch);
+
+            if (isRightBracket(ch)) {
+                if (stack.empty())
+                    return false;
+
+                var top = stack.pop();
+                if (!bracketsMatch(top, ch)) return false;
+            }
+        }
+        return stack.empty();
+    }
+
+     private boolean isLeftBracket(char ch) {
+        return leftBrackets.contains(ch);
+    }
+
+    private boolean isRightBracket(char ch) {
+        return rightBrackets.contains(ch);
+    }
+
+    private boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
+    }
+}
